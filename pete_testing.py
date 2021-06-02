6acbea0a8caba6a9b51cb0c191d13ec80a318d10
Peter Stenger <peter.a.stenger@gmail.com>
2021-06-01 23:45:42 -0400

    add gadget loading

--- pete_testing.py
+++ pete_testing.py
@@ -2,7 +2,7 @@ import angr
 import angrop
 import logging
 import inspect
-
+import os
 l = logging.getLogger('ctorop')
 
 
@@ -11,7 +11,13 @@ class ROP():
         p = angr.Project(file)
         angrop_rop = p.analyses.ROP()
         l.debug(f'Finding gadgets...')
-        angrop_rop.find_gadgets()
+        gadget_file = file.split('/')[-1] + '.gadgets'
+        if os.path.exists(gadget_file):
+            angrop_rop.load_gadgets(gadget_file)
+        else:
+            angrop_rop.find_gadgets()
+            l.debug(f'Saving gadgets...')
+            angrop_rop.save_gadgets(gadget_file)
         self.__rop = angrop_rop
         l.debug('Superclassing angrop...')
         for name, value in inspect.getmembers(self.__rop):
