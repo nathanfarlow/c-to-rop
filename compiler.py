8f1715a161b1c78ea4b37f7b25ffc05c8ae1c6ed
Nathan Farlow <natethegreatest11@gmail.com>
2021-07-02 22:51:44 -0500

    Add compiler template

--- compiler.py
+++ compiler.py
@@ -1,58 +1,40 @@
-import angr
-from angrop import angrop
 
-from dataclasses import dataclass
+class Compiler:
 
-import pickle
+    CONDITION_CODES = ['EQ', 'NE', 'LT', 'GT', 'LE', 'GE']
 
-@dataclass
-class Register:
-    name: str
-    # Can we load register from memory?
-    load_from_mem: angrop.rop_chain.RopChain = None
-    # Can we load register from immediate value?
-    load_from_imm: angrop.rop_chain.RopChain = None
-    # Can we write register to memory?
-    write: angrop.rop_chain.RopChain = None
+    def put_mov(dst, src):
+        pass
 
-registers = {name: Register(name) for name in ('rax', 'rbx', 'rcx', 'rdx', 'rsi',
-                                                'rdi', 'rbp', 'rsp', 'r8', 'r9',
-                                                'r10', 'r11', 'r12', 'r13', 'r14',
-                                                'r15')}
+    def put_add(dst, src):
+        pass
 
-def find_loadable_imm(rop):
-    for r in registers:
-        try:
-            registers[r].load_from_imm = rop.set_regs({r: 0xCAFEBABEDEADBEEF})
-        except:
-            pass
+    def put_sub(dst, src):
+        pass
 
-def find_loadable_mem(rop):
-    for r in registers:
-        try:
-            registers[r].load_from_mem = rop.read_mem_to_register(0xCAFEBABEDEADBEEF, r)
-        except Exception as e:
-            pass
+    def put_load(dst, src):
+        pass
 
-def find_writeable(rop):
-    for r in registers:
-        try:
-            registers[r].write = rop.write_register_to_mem(0xCAFEBABEDEADBEEF, r)
-        except Exception as e:
-            pass
+    def put_store(src, dst):
+        pass
 
-p = angr.Project('/bin/ls')
-rop = p.analyses.ROP()
-rop.find_gadgets()
+    def put_putc(src):
+        pass
 
-find_loadable_mem(rop)
-find_loadable_imm(rop)
-find_writeable(rop)
+    def put_getc(dst):
+        pass
 
-directly_loadable_mem = [r for r in registers if registers[r].load_from_mem]
-directly_loadable_imm = [r for r in registers if registers[r].load_from_imm]
-directly_writable = [r for r in registers if registers[r].write]
+    def put_exit(dst):
+        pass
 
-print(f'Directly loadable from memory: {directly_loadable_mem}')
-print(f'Directly loadable from immediate: {directly_loadable_imm}')
-print(f'Directly writable to memory: {directly_writable}')
+    def put_conditional_jmp(jmp, dst, src, cc):
+        pass
+
+    def put_unconditional_jmp(jmp):
+        pass
+
+    def put_cmp(dst, src, cc):
+        pass
+
+    def put_dump():
+        pass
