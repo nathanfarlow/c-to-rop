d5f60f8e14f2633d70969e66ccfedee6cd649c6b
Nathan Farlow <natethegreatest11@gmail.com>
2021-07-02 23:02:03 -0500

    Move tests to one file

--- test/add.asm
+++ test/add.asm
@@ -5,9 +5,24 @@ section .text
 
 _start:
 
+    ; syscall gadget
     mov rax, 60
     xor rdi, rdi
     syscall
+
+    ; Comparison gadgets
+    cmp rax, rbx
+    ret
+
+    sub rax, rbx
+    ret
+
+    ; sete + setl gadgets
+    sete al
+    ret
+
+    setl al
+    ret
 
     ; Add reg to mem gadget
     add [rbx], rax
@@ -19,4 +34,53 @@ _start:
 
     ; Add reg to reg gadget
     add rax, rbx
+    ret
+
+    ; Register setting gadgets
+    pop rax
+    ret
+
+    pop rbx
+    ret
+
+    pop rcx
+    ret
+
+    pop rdx
+    ret
+
+    pop rbp
+    ret
+
+    pop rsp
+    ret
+
+    pop rsi
+    ret
+
+    pop rdi
+    ret
+
+    pop r8
+    ret
+
+    pop r9
+    ret
+
+    pop r10
+    ret
+
+    pop r11
+    ret
+
+    pop r12
+    ret
+
+    pop r13
+    ret
+
+    pop r14
+    ret
+
+    pop r15
     ret
