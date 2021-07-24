5f3b7c003fd34607f8a20ebe187562c83350813a
Nathan Farlow <natethegreatest11@gmail.com>
2021-07-24 12:10:35 -0500

    Update chain finder
    Add find mov immediate to memory chains
    
    Fix bugs where gadgets would be included in wrong chains
    due to using same list reference in generator

--- chainfinder.py
+++ chainfinder.py
@@ -11,7 +11,8 @@ from angrop.rop_chain import RopChain
 
 class ParameterizedChain:
     
-    def __init__(self, rop: ROP, gadgets: list[tuple[ParameterizedGadget, str]] = None,  builder: Callable[..., RopChain] = None) -> None:
+    def __init__(self, rop: ROP, gadgets: list[tuple[ParameterizedGadget, str]] = None,
+                    builder: Callable[..., RopChain] = None) -> None:
         self.rop = rop
         self.gadgets = gadgets or []
         self.builder = builder
@@ -48,7 +49,7 @@ class ChainFinder:
     def __init__(self, gadgets: GadgetRepository) -> None:
         self.gadgets = gadgets
 
-    def _mov_reg_to_reg(self, dest: str, src: str, avoid=set()) -> Generator[ParameterizedChain]:
+    def _mov_reg_to_reg(self, dest: str, src: str, bits, avoid=set()) -> Generator[ParameterizedChain]:
 
         if dest in avoid:
             return
@@ -57,13 +58,16 @@ class ChainFinder:
             yield ParameterizedChain(self.gadgets.rop)
             return
 
-        registers_that_write_to_dest = filter(lambda to_from: to_from[0] == dest and len(self.gadgets.mov_register_to_register[to_from]) > 0, self.gadgets.mov_register_to_register)
+        registers_that_write_to_dest = filter(lambda to_from: to_from[0] == dest and len(self.gadgets.mov_register_to_register[bits][to_from]) > 0,
+                                                self.gadgets.mov_register_to_register[bits])
 
         for _, reg in registers_that_write_to_dest:
-            for init_chain in self._mov_reg_to_reg(reg, src, avoid | {dest}):
-                for mov_to_dest_gadget in self.gadgets.mov_register_to_register[(dest, reg)]:
-                    init_chain.add_gadget((mov_to_dest_gadget, None))
-                    yield init_chain
+            for init_chain in self._mov_reg_to_reg(reg, src, bits, avoid | {dest}):
+                for mov_to_dest_gadget in self.gadgets.mov_register_to_register[bits][(dest, reg)]:
+                    full_chain = ParameterizedChain(self.gadgets.rop)
+                    full_chain.add_all(init_chain.gadgets)
+                    full_chain.add_gadget((mov_to_dest_gadget, None))
+                    yield full_chain
 
     def find_mov_mem_to_mem(self) -> Generator[ParameterizedChain]:
 
@@ -76,10 +80,26 @@ class ChainFinder:
                 for reg_dest in self.gadgets.write_register_to_mem:
                     for write_reg_to_mem in self.gadgets.write_register_to_mem[reg_dest]:
 
-                        for mov_reg_to_reg in self._mov_reg_to_reg(reg_dest, reg_src):
+                        for mov_reg_to_reg in self._mov_reg_to_reg(reg_dest, reg_src, self.gadgets.rop.project.arch.bits):
                             result = ParameterizedChain(self.gadgets.rop, builder=build)
                             result.add_gadget((read_mem_to_reg, 'src'))
                             result.add_all(mov_reg_to_reg.gadgets)
                             result.add_gadget((write_reg_to_mem, 'dest'))
                             yield result
 
+    def find_mov_imm_to_mem(self) -> Generator[ParameterizedChain]:
+
+        def build(chain, dest, src):
+            return chain._apply_args({'dest': (dest,), 'src': (src,)})
+
+        for reg_src in self.gadgets.set_register_value:
+            for set_register_value in self.gadgets.set_register_value[reg_src]:
+                for reg_dest in self.gadgets.write_register_to_mem:
+                    for write_reg_to_mem in self.gadgets.write_register_to_mem[reg_dest]:
+
+                        for mov_reg_to_reg in self._mov_reg_to_reg(reg_dest, reg_src, self.gadgets.rop.project.arch.bits):
+                            result = ParameterizedChain(self.gadgets.rop, builder=build)
+                            result.add_gadget((set_register_value, 'src'))
+                            result.add_all(mov_reg_to_reg.gadgets)
+                            result.add_gadget((write_reg_to_mem, 'dest'))
+                            yield result
